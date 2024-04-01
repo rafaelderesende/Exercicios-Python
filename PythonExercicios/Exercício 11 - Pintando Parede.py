93caaad32107ccce674eb21b146f4fb9c0b3b80b
Rresende <rafael.resenderr1@gmail.com>
2024-04-01 13:59:54 -0300

    All Program codes created

--- PythonExercicios/Exercício 11 - Pintando Parede.py
+++ PythonExercicios/Exercício 11 - Pintando Parede.py
@@ -3,5 +3,5 @@ Altura_P = float(input('Informe a altura de sua parede m: ' ))
 LTinta = 1/2
 Area_T = Largura_P * Altura_P
 QTinta = LTinta*Area_T
-print(' Sua área total de parede é de {}m2'.format(Area_T))
-print(' Você precisará de {}lts de tinta para pintar {}m2 de parede'.format(QTinta, Area_T))
+print(' Sua área total de parede é de {:.2f}m2'.format(Area_T))
+print(' Você precisará de {:.2f}lts de tinta para pintar {:.2f}m2 de parede'.format(QTinta, Area_T))
